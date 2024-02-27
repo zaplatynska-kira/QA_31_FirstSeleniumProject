a3ef3c43a154e71805bef6ff50552dd7cba2f8ba
Kira <zaplatynskakira@gmail.com>
2024-02-27 07:31:29 +0100

    Homework_05

---
+++ src/test/java/com/ait/qa31/AddToCartTest.java
@@ -0,0 +1,46 @@
+package com.ait.qa31;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AddToCartTest extends TestBase {
+
+    @BeforeMethod
+    public void login() {
+        if (!isElementPresent(By.cssSelector(".ico-logout"))) {
+
+            driver.findElement(By.cssSelector(".ico-login")).click();
+            driver.findElement(By.id("Email")).sendKeys("bell@gm.com");
+            driver.findElement(By.id("Password")).sendKeys("Bell007$");
+            driver.findElement(By.cssSelector("input[value='Log in']")).click();
+        }
+    }
+
+    @Test
+    public void addToCartTest() {
+
+        String productName = "14.1-inch Laptop";
+
+        if (isElementPresent(By.cssSelector(".cart .product-name"))) {
+            driver.findElement(By.cssSelector(".header-logo")).click();
+        }
+
+        driver.findElement(By.linkText(productName)).click();
+
+        driver.findElement(By.cssSelector(".add-to-cart-button")).click();
+
+
+        driver.findElement(By.cssSelector(".ico-cart")).click();
+
+
+        Assert.assertTrue(isElementPresent(By.cssSelector(".cart .product-name")));
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown(){
+        driver.close();
+    }
+}
